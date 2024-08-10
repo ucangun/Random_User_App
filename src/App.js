094198e94dbb97bbf682d9c6f9b451132b1e983f
ucangun <149247682+ucangun@users.noreply.github.com>
2024-08-10 15:54:11 +0200

    "Updated App.js to import SVG assets and use them in buttons, and modified index.css to style the buttons"

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
+import mail from "./assets/mail.svg";
+import map from "./assets/map.svg";
+import phone from "./assets/phone.svg";
+import ageMan from "./assets/growing-up-man.svg";
+import ageWoman from "./assets/growing-up-woman.svg";
+import man from "./assets/man.svg";
+import woman from "./assets/woman.svg";
+import lock from "./assets/padlock.svg";
+
 function App() {
   const url = "https://randomuser.me/api/";
 
@@ -24,18 +33,38 @@ function App() {
         <div className="card">
           <img src={userData?.picture.large} alt="user" />
           <div className="user">
-            <h4 className="user_desc">fc</h4>
+            <h4 className="user_desc">My Name is</h4>
             <h3 className="user_info">{`${userData?.name.first} ${userData?.name.last}`}</h3>
           </div>
           <div className="buttons">
-            <button>name</button>
-            <button>email</button>
-            <button>age</button>
-            <button>location</button>
-            <button>tel</button>
-            <button>password</button>
+            <button>
+              {userData?.gender === "male" ? (
+                <img src={man} alt="man" />
+              ) : (
+                <img src={woman} alt="woman" />
+              )}
+            </button>
+            <button>
+              <img src={mail} alt="" />
+            </button>
+            <button>
+              {userData?.gender === "male" ? (
+                <img src={ageMan} alt="age man" />
+              ) : (
+                <img src={ageWoman} alt="age woman" />
+              )}
+            </button>
+            <button>
+              <img src={map} alt="" />
+            </button>
+            <button>
+              <img src={phone} alt="" />
+            </button>
+            <button>
+              <img src={lock} alt="" />
+            </button>
           </div>
-          <div className="buttons">
+          <div>
             <button>New User</button>
             <button>Add User</button>
           </div>
