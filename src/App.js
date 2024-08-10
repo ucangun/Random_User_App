818e0386d6f5853016f10286c8c8e258f60a1977
ucangun <149247682+ucangun@users.noreply.github.com>
2024-08-10 14:51:08 +0200

    "Updated App.js to add HTML structure and styles to index.css for user card component"

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <header>
+        <h1>Random User App</h1>
+      </header>
+      <main>
+        <div className="card">
+          <img src="" alt="" />
+          <div className="user">
+            <h4 className="user_desc">User Desc</h4>
+            <h3 className="user_info">Info</h3>
+          </div>
+          <div className="buttons">
+            <button>name</button>
+            <button>email</button>
+            <button>age</button>
+            <button>location</button>
+            <button>tel</button>
+            <button>password</button>
+          </div>
+          <div className="buttons">
+            <button>New User</button>
+            <button>Add User</button>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default App;
